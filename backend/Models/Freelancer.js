681f098e736512ab3f36dfb7093242d464ed2295
“Russel <russeldanielpaul@gmail.com>
2025-02-22 22:15:12 +0530

    Russel

---
+++ backend/Models/Freelancer.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const FreelancerSchema = new mongoose.Schema({
+  firebaseUID: { type: String, required: true, unique: true }, // Firebase UID for authentication
+  bio: {type: String, required: true, unique: true},
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  profilePicture: { type: String },
+  resume: { type: String, required: true },
+  skills: { type: [String], required: true }, // List of skills
+  portfolio: { type: String }, // Links to portfolio projects
+  hourlyRate: { type: Number, required: true }, // Hourly rate in dollars
+  availability: { type: String, enum: ["full-time", "part-time", "freelance"], default: "freelance" },
+  paymentMethod: { type: [String], enum: ["PayPal", "Stripe", "Bank Transfer"], default: ["PayPal"] },
+  github: { type: String},
+  rating: {type: Number, required: true, default: 0},
+  reviews: {type: Number, required: true, default: 0}
+}, {timestamps: true});
+
+module.exports = mongoose.model("Freelancer", FreelancerSchema);
