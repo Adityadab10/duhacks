7dc741ce3ac13c24395aaa2263e6af3ec2139465
“Russel <russeldanielpaul@gmail.com>
2025-02-22 14:16:14 +0530

    hisjcqsakxn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,6 @@ function App() {
   return (
     <>
       <Navbar/>
-      {/* <HeroSection/> */}
-      {/* ..My name is Russel  */}
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-      <h2 className='text-3xl font-bold underline'>Hello Worldd</h2>
-      <Navbar/>
       <HeroSection/>
     </>
   )
