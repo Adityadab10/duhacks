00469cb8d61f3e96284b09b9fad4a75b14e653e1
Adityadab10 <adityadab27@gmail.com>
2025-02-22 14:24:31 +0530

    comm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
       <HeroSection/>
-    </>
+    </Router>
   )
 }
 
