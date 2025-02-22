adf56ca1b9e8da0ac3aa1dec811b8b5e18a40d22
Adityadab10 <adityadab27@gmail.com>
2025-02-22 14:13:22 +0530

    hii

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,8 @@ import HeroSection from './components/HeroSection'
 function App() {
   return (
     <>
-      {/* <Navbar/>
-      <HeroSection/> */}
+      <Navbar/>
+      {/* <HeroSection/> */}
       {/* ..My name is Russel  */}
       <h1 className='text-3xl font-bold underline'>Hello World</h1>
       <h2 className='text-3xl font-bold underline'>Hello Worldd</h2>
