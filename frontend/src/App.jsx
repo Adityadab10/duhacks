ce28f3592708ea9279b470fce6f77802d18fc451
Adityadab10 <adityadab27@gmail.com>
2025-02-22 14:53:01 +0530

    make section on homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
-
+import Section from './components/Section'
 import FreelancerLogin from "./components/Freelancer/FreelancerLogin"
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <HeroSection/>
-      
+      <Routes>
+        <Route path="/" element={
+          <>
+            <HeroSection/>
+            <Section/>
+          </>
+        } />
+        <Route path="/freelancer-login" element={<FreelancerLogin />} />
+      </Routes>
     </Router>
   )
 }
